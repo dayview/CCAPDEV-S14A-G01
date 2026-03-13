5cc2b34b546ddc0157f383f32514a5ea975722d7
massimocolcol <colcolmassi@gmail.com>
2026-03-13 21:13:17 +0800

    fix redirect of edit search and delete and fix bugs
    note that not using deletereservation.js anymore its still there jic

--- src/routes/reservation_routes.js
+++ src/routes/reservation_routes.js
@@ -8,6 +8,10 @@ router.get('/', ctrl.getReservationOverview);
 router.get('/student', requireStudent, ctrl.getStudentReservation);
 router.post('/student', requireStudent, ctrl.postStudentReservation);
 
+router.get('/search', requireStudent, ctrl.getSearchPage);
+router.get('/edit', requireStudent, ctrl.getEditPage);
+router.get('/delete', requireStudent, ctrl.getDeletePage);
+
 router.get('/edit/:id', requireStudent, ctrl.getEditReservation);
 router.post('/edit/:id', requireStudent, ctrl.postEditReservation);
 router.post('/delete/:id', requireStudent, ctrl.postDeleteReservation);
