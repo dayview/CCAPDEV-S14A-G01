fa85d2959a182dc6d3da473b26932c218a9e65a1
massimocolcol <165057595+massimocolcol@users.noreply.github.com>
2026-03-13 23:22:46 +0800

    Update reservation_routes.js

--- src/routes/reservation_routes.js
+++ src/routes/reservation_routes.js
@@ -8,8 +8,15 @@ router.get('/', ctrl.getReservationOverview);
 router.get('/student', requireStudent, ctrl.getStudentReservation);
 router.post('/student', requireStudent, ctrl.postStudentReservation);
 
+router.get('/search', requireStudent, ctrl.getSearchPage);
+router.get('/search-availability', requireStudent, ctrl.searchAvailability);
+
+router.get('/edit', requireStudent, ctrl.getEditPage);
+router.get('/delete', requireStudent, ctrl.getDeletePage);
+
 router.get('/edit/:id', requireStudent, ctrl.getEditReservation);
 router.post('/edit/:id', requireStudent, ctrl.postEditReservation);
 router.post('/delete/:id', requireStudent, ctrl.postDeleteReservation);
+
 
 module.exports = router;
