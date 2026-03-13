dad6f1254ee36a2035b0d24b457963526855b37c
massimocolcol <colcolmassi@gmail.com>
2026-03-13 23:04:05 +0800

    update

--- src/routes/reservation_routes.js
+++ src/routes/reservation_routes.js
@@ -9,11 +9,14 @@ router.get('/student', requireStudent, ctrl.getStudentReservation);
 router.post('/student', requireStudent, ctrl.postStudentReservation);
 
 router.get('/search', requireStudent, ctrl.getSearchPage);
+router.get('/search-availability', requireStudent, ctrl.searchAvailability);
+
 router.get('/edit', requireStudent, ctrl.getEditPage);
 router.get('/delete', requireStudent, ctrl.getDeletePage);
 
 router.get('/edit/:id', requireStudent, ctrl.getEditReservation);
 router.post('/edit/:id', requireStudent, ctrl.postEditReservation);
 router.post('/delete/:id', requireStudent, ctrl.postDeleteReservation);
+
 
 module.exports = router;
