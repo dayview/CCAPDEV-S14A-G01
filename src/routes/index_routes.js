0d52d60d69dbe4ec278722b3bebab4c78aa20c52
cuefront <131971014+dayview@users.noreply.github.com>
2026-03-21 01:23:57 +0800

    add: about page with npm packages list and nav link

--- src/routes/index_routes.js
+++ src/routes/index_routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const ctrl = require('../controllers/index_controller');
 
 router.get('/', ctrl.getIndex);
+router.get('/about', (req, res) => res.render('about'));
 
 module.exports = router;
