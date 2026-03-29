dc92702e5a1252bec7f130af9f2a936be37d10eb
massimocolcol <165057595+massimocolcol@users.noreply.github.com>
2026-03-30 01:22:19 +0800

    Add files via upload

--- public/js/user_profile.js
+++ public/js/user_profile.js
@@ -5,3 +5,26 @@ document.addEventListener("DOMContentLoaded", () => {
         window.location.assign("/auth/logout");
     })
 })
+
+document.addEventListener("DOMContentLoaded", () => {
+    const logoutBtn = document.getElementById("logout_btn");
+    if (logoutBtn) {
+        logoutBtn.addEventListener("click", function () {
+            window.location.assign("/auth/logout");
+        });
+    }
+    const profileInput = document.getElementById("profilePicture");
+    const profilePreview = document.querySelector(".profile-pic-preview");
+    if (profileInput && profilePreview) {
+        profileInput.addEventListener("change", function(event) {
+            const file = event.target.files[0];        
+            if (file) {   
+                const reader = new FileReader();
+                reader.onload = function(e) {
+                    profilePreview.src = e.target.result; 
+                }
+                reader.readAsDataURL(file);
+            }
+        });
+    }
+});
